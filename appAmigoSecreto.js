d2635430ee690548421cea34f14dbe0556fb910e
lucass_bell <lucasbell275@gmail.com>
2025-08-19 17:10:02 -0300

    Adicion funcion

--- appAmigoSecreto.js
+++ appAmigoSecreto.js
@@ -3,29 +3,31 @@ function textHTML(elemento, texto){
     elementoHTML.innerHTML = texto;
     return;
 }
-let listaDeAmigos = [];
+
+
+let listaDeAmigos = []; 
+
 
 function limpiarCaja(){
-    document.getElementById('amigo').value = ''
+    document.getElementById('amigo').value = '' //Tipica funcion de limpiar la caja
 }
-function actualizarLista(){
 
-}
-var nombres = document.getElementById('amigo')
-var valorNombre = nombres.value
+/*Esta es la variable que toma el ID del campo de texto, 
+pero no puede capturar el valor. Tiene que hacerse en orden*/
+let nombres = document.getElementById('amigo')
 function aggAmigos(){
-    //var valorNombre = nombres.value
-    if(valorNombre == ''){ 
+     /*Importante aclarar, la variable que captura el valor de los nombres tiene que ir adentro de la funcion,
+      para que CADA VEZ que se clickea en el boton, se actualice el valor de la variable y lo capture.
+      Si no siempre va a capturar un espacio vacio.*/
+    let valorNombre = nombres.value
+    if(valorNombre == ''){ //Esto se usa por si no se ingresa ningun resultado y no se agreguen valores vacios.
         alert('Por favor, inserte un nombre.');
     } else{
-        listaDeAmigos.push(valorNombre);
+        listaDeAmigos.push(valorNombre); //Aca cada vez que es correcto el nombre ingresado, se ingresa a la lista previamente declarada 'listaDeAmigos'
     }
     console.log(listaDeAmigos);
 }
-console.log(listaDeAmigos)
-/*function aggAmigos(nombre){
-            listaAmigoss.push(nombre);
-            limpiarCaja()
-    }
-console.log(listaAmigoss)*/
 
+function actualizarLista(){
+    for(let veces = 0, )
+}
